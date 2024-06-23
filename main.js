1fc4b70e03e9aaf6fe812b9c0bcf0de0f2103386
Mateja <stanojevicmateja@gmail.com>
2024-06-24 01:17:13 +0200

    macko promene

--- main.js
+++ main.js
@@ -2,198 +2,227 @@ const countries = {
   norway: {
     name: 'Norway',
     societies: [{
-      name: 'norway',
-      link: 'norway'
-    }]
+      name: 'Norwegian Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/norwegian-psychoanalytical-society',
+    }],
   },
   russia: {
     name: 'Russia',
     societies: [{
-      name: 'russia',
-      link: 'russia'
-    }]
+      name: 'Moscow Psychoanalytic Society',
+      link: 'https://www.epf-fep.eu/en/society/moscow-psychoanalytic-society',
+    }, {
+      name: 'Moscow Group of Psychoanalysts',
+      link: 'https://www.epf-fep.eu/en/society/moscow-group-of-psychoanalysts',
+    }],
   },
   sweden: {
     name: 'Sweden',
     societies: [{
-      name: 'sweden',
-      link: 'sweden'
-    }]
+      name: 'Swedish Psychoanalytical Association',
+      link: 'https://www.epf-fep.eu/en/society/swedish-psychoanalytical-association',
+    }],
   },
   finland: {
     name: 'Finland',
     societies: [{
-      name: 'finland',
-      link: 'finland'
-    }]
+      name: 'Finnish Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/finnish-psychoanalytical-society',
+    }],
   },
   'estonia-latvia': {
     name: 'Estonia & Latvia',
     societies: [{
-      name: 'estonia-latvia',
-      link: 'estonia-latvia'
-    }]
+      name: 'Estonian-Latvian Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/estonian-latvian-psychoanalytical-society',
+    }],
   },
   lithuania: {
     name: 'Lithuania',
     societies: [{
-      name: 'lithuania',
-      link: 'lithuania'
-    }]
+      name: 'Vilnius Society of Psychoanalysts',
+      link: 'https://www.epf-fep.eu/en/society/vilnius-society-of-psychoanalysts',
+    }],
   },
   ukraine: {
     name: 'Ukraine',
     societies: [{
-      name: 'ukraine',
-      link: 'ukraine'
-    }]
+      name: 'Ukraine Psychoanalytic Society',
+      link: 'https://www.epf-fep.eu/en/society/ukraine-psychoanalytic-society',
+    }],
   },
   romania: {
     name: 'Romania',
     societies: [{
-      name: 'romania',
-      link: 'romania'
-    }]
+      name: 'Romanian Society of Psychoanalysis',
+      link: 'https://www.epf-fep.eu/en/society/romanian-society-of-psychoanalysis',
+    }],
   },
   bulgaria: {
     name: 'Bulgaria',
     societies: [{
-      name: 'bulgaria',
-      link: 'bulgaria'
-    }]
+      name: 'Bulgarian Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/bulgarian-psychoanalytic-society-bps',
+    }],
   },
   greece: {
     name: 'Greece',
     societies: [{
-      name: 'greece',
-      link: 'greece'
-    }]
+      name: 'Hellenic Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/hellenic-psychoanalytical-society',
+    }],
   },
   serbia: {
     name: 'Serbia',
     societies: [{
-      name: 'serbia',
-      link: 'serbia'
-    }]
+      name: 'Belgrade Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/belgrade-psychoanalytical-society',
+    }, {
+      name: 'Psychoanalytical Society of Serbia',
+      link: 'https://www.epf-fep.eu/en/society/psychoanalytical-society-of-serbia',
+    }],
   },
   croatia: {
     name: 'Croatia',
     societies: [{
-      name: 'croatia',
-      link: 'croatia'
-    }]
+      name: 'Croatian Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/croatian-psychoanalytical-society',
+    }],
   },
   hungary: {
     name: 'Hungary',
     societies: [{
-      name: 'hungary',
-      link: 'hungary'
-    }]
+      name: 'Hungarian Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/hungarian-psychoanalytical-society',
+    }],
   },
   italy: {
     name: 'Italy',
     societies: [{
-      name: 'italy',
-      link: 'italy'
-    }]
+      name: 'Italian Psychoanalytical Association/AIPsi',
+      link: 'https://www.epf-fep.eu/en/society/italian-psychoanalytical-association-aipsi',
+    }, {
+      name: 'Italian Psychoanalytical Society/SPI',
+      link: 'https://www.epf-fep.eu/en/society/italian-psychoanalytical-society-spi',
+    }],
   },
   turkey: {
     name: 'Turkey',
     societies: [{
-      name: 'turkey',
-      link: 'turkey'
-    }]
+      name: 'Istanbul Psychoanalytical Association',
+      link: 'https://www.epf-fep.eu/en/society/istanbul-psychoanalytical-association',
+    }, {
+      name: 'Psike İstanbul Psychoanalytic Association',
+      link: 'https://www.epf-fep.eu/en/society/psike-istanbul-psychoanalytic-association',
+    }],
   },
   spain: {
     name: 'Spain',
     societies: [{
-      name: 'spain',
-      link: 'spain'
-    }]
+      name: 'Madrid Psychoanalytical Association',
+      link: 'https://www.epf-fep.eu/en/society/madrid-psychoanalytical-association',
+    }, {
+      name: 'Spanish Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/spanish-psychoanalytical-society',
+    }],
   },
   portugal: {
     name: 'Portugal',
     societies: [{
-      name: 'portugal',
-      link: 'portugal'
-    }]
+      name: 'Portuguese Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/portuguese-psychoanalytical-society',
+    }, {
+      name: 'Portuguese Nucleus of Psychoanalysis',
+      link: 'https://www.epf-fep.eu/en/society/portuguese-nucleus-of-psychoanalysis',
+    }],
   },
   switzerland: {
     name: 'Switzerland',
     societies: [{
-      name: 'switzerland',
-      link: 'switzerland'
-    }]
+      name: 'Swiss Society of Psychoanalysis',
+      link: 'https://www.epf-fep.eu/en/society/swiss-society-of-psychoanalysis',
+    }],
   },
   austria: {
     name: 'Austria',
     societies: [{
-      name: 'austria',
-      link: 'austria'
-    }]
+      name: 'Vienna Psychoanalytic Association',
+      link: 'https://www.epf-fep.eu/en/society/vienna-psychoanalytic-association',
+    }, {
+      name: 'Vienna Psychoanalytic Society',
+      link: 'https://www.epf-fep.eu/en/society/vienna-psychoanalytic-society',
+    }],
   },
   germany: {
     name: 'Germany',
     societies: [{
-      name: 'germany',
-      link: 'germany'
-    }]
+      name: 'German Psychoanalytical Association/DPV',
+      link: 'https://www.epf-fep.eu/en/society/german-psychoanalytical-association-dpv',
+    }, {
+      name: 'German Psychoanalytical Society/DPG',
+      link: 'https://www.epf-fep.eu/en/society/german-psychoanalytical-society',
+    }],
   },
   denmark: {
     name: 'Denmark',
     societies: [{
-      name: 'denmark',
-      link: 'denmark'
-    }]
+      name: 'Danish Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/danish-psychoanalytical-society',
+    }],
   },
   'czech-republic': {
     name: 'Czech Republic',
     societies: [{
-      name: 'czech-republic',
-      link: 'czech-republic'
-    }]
+      name: 'Czech Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/czech-psychoanalytical-society',
+    }],
   },
   poland: {
     name: 'Poland',
     societies: [{
-      name: 'poland',
-      link: 'poland'
-    }]
+      name: 'Polish Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/polish-psychoanalytical-society',
+    }],
   },
   france: {
     name: 'France',
     societies: [{
-      name: 'france',
-      link: 'france'
-    }]
+      name: 'French Psychoanalytical Association/APF',
+      link: 'https://www.epf-fep.eu/en/society/french-psychoanalytical-association-apf',
+    }, {
+      name: 'Paris Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/paris-psychoanalytical-society',
+    }, {
+      name: 'Psychoanalytic Society for Research and Training',
+      link: 'https://www.epf-fep.eu/en/society/psychoanalytic-society-for-research-and-training',
+    }],
   },
   'the-netherlands': {
     name: 'The Netherlands',
     societies: [{
-      name: 'the-netherlands',
-      link: 'the-netherlands'
-    }]
+      name: 'Dutch Psychoanalytic Society',
+      link: 'https://www.epf-fep.eu/en/society/dutch-psychoanalytic-society',
+    }],
   },
   belgium: {
     name: 'Belgium',
     societies: [{
-      name: 'belgium',
-      link: 'belgium'
-    }]
-  },
-  'fixme-beneath-belgium': {
-    name: 'Fixme Beneath Belgium',
-    societies: [{
-      name: 'fixme-beneath-belgium',
-      link: 'fixme-beneath-belgium'
-    }]
+      name: 'Belgian Psychoanalytic Society',
+      link: 'https://www.epf-fep.eu/en/society/belgian-psychoanalytic-society',
+    }],
   },
   'the-united-kingdom': {
     name: 'The United Kingdom',
     societies: [{
-      name: 'the-united-kingdom',
-      link: 'the-united-kingdom'
-    }]
+      name: 'British Psychoanalytic Association',
+      link: 'https://www.epf-fep.eu/en/society/british-psychoanalytic-association',
+    }, {
+      name: 'British Psychoanalytical Society',
+      link: 'https://www.epf-fep.eu/en/society/british-psychoanalytical-society',
+    }, {
+      name: 'Northern Ireland Psychoanalytic Society',
+      link: 'https://www.epf-fep.eu/en/society/northern-ireland-psychoanalytic-society',
+    }],
   },
 };
 
@@ -208,13 +237,39 @@ const showCountryInfo = (name, societies) => {
   societies.forEach((society) => {
     const link = document.createElement('a');
     link.href = society.link;
+    link.target = '_blank';
     link.textContent = society.name;
+    link.addEventListener('click', (e) => e.stopPropagation());
     societyLinks.appendChild(link);
     societyLinks.appendChild(document.createElement('br'));
   });
 
   infoDiv.classList.add('active');
 }
+
+const drawBezierCurve = (country) => {
+  const elem1 = document.querySelector('.country-info-container');
+  const elem2 = document.getElementById(country);
+
+  const rect1 = elem1.getBoundingClientRect();
+  const rect2 = elem2.getBoundingClientRect();
+
+  const startX = rect1.left + rect1.width / 2;
+  const startY = rect1.top;
+
+  const controlPoint1X = rect1.left + rect1.width / 2;
+  const controlPoint1Y = rect1.top + rect1.height;
+
+  const controlPoint2X = rect2.left + rect2.width / 2;
+  const controlPoint2Y = rect2.top;
+
+  const endX = rect2.left;
+  const endY = rect2.top;
+
+  const bezierCurvePath = document.getElementById('bezierCurve');
+  const pathData = `M${startX},${startY} C${controlPoint1X},${controlPoint1Y} ${controlPoint2X},${controlPoint2Y} ${endX},${endY}`;
+  bezierCurvePath.setAttribute('d', pathData);
+};
 
 let selectedCountryElement = null;
 
